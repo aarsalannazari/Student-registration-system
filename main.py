0a1bb4aa1f535ff7a34c503648f6bd1e30949a51
aarsalannazari <aarsalannazari@gmail.com>
2023-01-10 22:41:02 +0000

    the second edit of defind the program

--- main.py
+++ main.py
@@ -1,3 +1,73 @@
 from tkinter import *
 from Data_Base_function import *
+
+window=Tk() # Create instance
+
+# Labels 
+name_label=Label(window,text='name:')
+name_label.grid(row=0,column=0,padx=5,pady=5)
+
+familly_label=Label(window,text='familly name:')
+familly_label.grid(row=0,column=2,padx=5,pady=5)
+
+student_id_label=Label(window,text='student id:')
+student_id_label.grid(row=1,column=0,padx=5,pady=5)
+
+age_label=Label(window,text='age:')
+age_label.grid(row=1,column=2,padx=5,pady=5)
+
+# Entrys
+
+name_entery=Entry(window)
+name_entery.grid(row=0,column=1)
+
+familly_entery=Entry(window)
+familly_entery.grid(row=0,column=3)
+
+id_entery=Entry(window)
+id_entery.grid(row=1,column=1)
+
+age_entery=Entry(window)
+age_entery.grid(row=1,column=3)
+
+search_entry=Entry(window)
+search_entry.grid(row=4,column=3)
+
+# Buttons
+
+add_button=Button(window,text='add')
+add_button.grid(row=3,column=0,padx=5,pady=5)
+
+edit_button=Button(window,text='edit')
+edit_button.grid(row=3,column=1,padx=5,pady=5)
+
+delete_button=Button(window,text='delete')
+delete_button.grid(row=3,column=2,padx=5,pady=5)
+
+show_button=Button(window,text='show all')
+show_button.grid(row=3,column=3,padx=5,pady=5)
+
+search_button=Button(window,text='search')
+search_button.grid(row=4,column=2,padx=5,pady=5)
+
+
+# Listbox
+
+listbox=Listbox(window,width=35,height=30)
+listbox.grid(row=4,column=1,rowspan=1,columnspan=1)
+
+# Scrollbar
+
+scrollbar=Scrollbar(window)
+scrollbar.grid(row=4,column=0)
+
+# Settings
+window.title('Student Registration System')
+window.geometry('700x700')
+window.resizable(width=False,height=False)
+
+# Run
+
+window.mainloop()
+
 # Continue this program for tomorrow
